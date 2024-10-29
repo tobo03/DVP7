0717b11d73805ae2416c089328877e81506263e2
Grigori II <rumi.adam@gmail.com>
2024-10-29 10:51:39 +0100

    Update BatchCreation.py

--- HelpfulFunctions/BatchCreation.py
+++ HelpfulFunctions/BatchCreation.py
@@ -9,8 +9,8 @@ def CreateBatch(X_tensor: torch.Tensor, y_train: np.ndarray, batch_size: int) ->
     
     Parameters
     ----------
-        X_tensor (torch.Tensor) : The input tensor with all data samples.
-        y_train (np.ndarray) : The labels corresponding to the samples in X_tensor.
+        X_tensor (torch.Tensor) : The input tensor with all data samples. Shape should be (sample_size, features).
+        y_train (np.ndarray) : The labels corresponding to the samples in X_tensor. Shape should be (sample_size, 1).
         batch_size (int) : The size of the batch to create.
  
     Returns
@@ -18,8 +18,8 @@ def CreateBatch(X_tensor: torch.Tensor, y_train: np.ndarray, batch_size: int) ->
         X_batch (torch.Tensor) : A tensor of shape (batch_size, features).
         y_batch (np.ndarray) : An array of shape (batch_size, 1).
     """
-    batch_i = random.sample(range(len(X_tensor)), batch_size) # Create batch_size random samples from X_tensor
-    X_batch = torch.stack( [X_tensor[j] for j in batch_i] ) #Create X_batch as torch tensor -> (batch_size, features)
+    batch_i = random.sample(range(len(X_tensor)), batch_size)
+    X_batch = torch.stack( [X_tensor[j] for j in batch_i] )
     y_batch = [y_train[j] for j in batch_i]
 
     return X_batch, y_batch
