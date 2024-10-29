4843e702a3159f2307ce4ae37ea0fc51909a366a
Grigori II <rumi.adam@gmail.com>
2024-10-29 09:32:50 +0100

    Start of Metrics file

---
+++ HelpfulFunctions/Metrics.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tqdm
+
+def mean_average_precision(test_hashes, training_hashes, test_labels, training_labels, compare_n):
+    aps = []
+    training_hashes = training_hashes[:compare_n,:]
+    training_labels = training_labels[:compare_n]
+    for i, test_hash in enumerate(tqdm(test_hashes)):
+        label = test_labels[i]
+        distances = np.abs(training_hashes - test_hashes[i]).sum(axis=1)
+        tp = np.where(training_labels==label, 1, 0)
+        hash_df = pd.DataFrame({"distances":distances, "tp":tp}).reset_index()
+        hash_df = hash_df.drop(index=i)
+        hash_df = hash_df.sort_values(["distances", "index"]).reset_index(drop=True)
+        hash_df = hash_df.drop(["index", "distances"], axis=1).reset_index()
+        hash_df = hash_df[hash_df["tp"]==1]
+        hash_df["tp"] = hash_df["tp"].cumsum()
+        hash_df["index"] = hash_df["index"] +1 
+        precision = np.array(hash_df["tp"]) / np.array(hash_df["index"])
+        ap = precision.mean()
+        aps.append(ap)
+    
+    return np.array(aps).mean()
